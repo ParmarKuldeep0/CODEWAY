431886de2c1b754f6c6589de9aa24aae8400d22c
ParmarKuldeep0 <152050428+ParmarKuldeep0@users.noreply.github.com>
2024-01-30 14:32:33 +0530

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-package CODEWAY.Task_01;
-
-public class Main {
-    public static void main(String[] args) {
-        NumberGame numberGame = new NumberGame();
-        numberGame.game();
-    }
-}
