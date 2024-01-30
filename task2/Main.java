b01a35f9784b27f11507dd215509fe7c5564144d
ParmarKuldeep0 <152050428+ParmarKuldeep0@users.noreply.github.com>
2024-01-30 19:50:04 +0530

    Add files via upload
    task_02

---
+++ task2/Main.java
@@ -0,0 +1,12 @@
+package CODEWAY.Task_01.task2;
+
+import java.util.Arrays;
+
+public class Main  {
+    public static void main(String[] args) {
+        studentsGrade calculator = new studentsGrade();
+        calculator.getGrades();
+        calculator.results();
+
+    }
+}
