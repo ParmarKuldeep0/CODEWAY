7aac4511c79f7d580f70b91be7251ece400812b8
ParmarKuldeep0 <parmark2076@gmail.com>
2024-01-30 14:15:06 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,8 @@
+package CODEWAY.Task_01;
+
+public class Main {
+    public static void main(String[] args) {
+        NumberGame numberGame = new NumberGame();
+        numberGame.game();
+    }
+}
